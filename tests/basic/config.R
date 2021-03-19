1b9661a012ba2e694a98ac2d4397fd16ac137561
Albert Brouwer <brouwer@iiasa.ac.at>
2021-03-19 09:35:18 +0100

    Add basic test

---
+++ tests/basic/config.R
@@ -0,0 +1,20 @@
+# Use paths relative to the working directory, with / as path separator.
+EXPERIMENT = "basic" # label for your run, pick something short but descriptive without spaces and valid as part of a filename
+PREFIX = "_condor" # prefix for per-job .err, log, .lst, and .out files
+JOBS = c(0:19)
+HOST_REGEXP = "^limpopo" # a regular expression to select execute hosts from the cluster
+REQUEST_MEMORY = 1000 # memory (MiB) to reserve for each job
+REQUEST_CPUS = 1 # number of hardware threads to reserve for each job
+LAUNCHER = "Rscript" # interpreter with which to launch the script
+SCRIPT = "test.R" # script that comprises your job
+ARGUMENTS = "%1" # arguments to the script
+BUNDLE_INCLUDE_DIRS = c("input") # recursive, supports wildcards
+BUNDLE_EXCLUDE_DIRS = c("Condor", "output") # recursive, supports wildcards
+BUNDLE_EXCLUDE_FILES = c("*.log") # supports wildcards
+BUNDLE_ADDITIONAL_FILES = c() # additional files to add to root of bundle, can also use an absolute path for these
+RETAIN_BUNDLE = FALSE
+GET_OUTPUT = TRUE
+OUTPUT_DIR = "output" # relative to working dir both host-side and on the submit machine
+OUTPUT_FILE = "output.RData" # as produced by a job on the execute-host, will be remapped with EXPERIMENT and cluster/job numbers to avoid name collisions when transferring back to the submit machine.
+WAIT_FOR_RUN_COMPLETION = TRUE
+CONDOR_DIR = "Condor" # directory where Condor reference files are stored in a per-experiment subdirectory (.err, .log, .out, .job and so on files)
