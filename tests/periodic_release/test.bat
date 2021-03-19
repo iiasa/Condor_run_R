250adbec556c42e56be35df07bd6b2a8cc8d786d
Albert Brouwer <brouwer@iiasa.ac.at>
2021-03-19 16:22:26 +0100

    Skip generating stats when an error occurs

--- tests/periodic_release/test.bat
+++ tests/periodic_release/test.bat
@@ -1,17 +1,20 @@
 :<<BATCH
 @echo off
 pushd %~dp0
-Rscript ..\..\Condor_run_basic.R config.R
+Rscript ..\..\Condor_run_basic.R config.R || (
+    popd
+    exit /b 1
+)
 Rscript ..\..\Condor_run_stats.R config.R
 popd
 exit /b :: end batch script processing
 BATCH
 # Platform-agnostic script.
-# Put batch commands above and functionally-identical Linux/MacOS shell
-# commands terminated by a # below. Save this script with CR+LF end-of-line
-# breaks. The trailing # makes the shell ignore the CR.
-# Must be run with the bash shell.
+# Put batch commands above and functionally-identical Linux/MacOS shell commands
+# terminated by a # below. When saving this script with CR+LF end-of-line breaks,
+# the trailing # makes the shell ignore the CR. Must be run with the bash shell.
 pushd "$(dirname "$0")" #
+set -e #
 Rscript ../../Condor_run_basic.R config.R #
 Rscript ../../Condor_run_stats.R config.R #
 popd #
