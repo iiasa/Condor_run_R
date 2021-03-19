016ee960e74a28e764158e27e86c4096c20d7716
Albert Brouwer <brouwer@iiasa.ac.at>
2021-03-19 17:29:58 +0100

    Add test of seeding inspite of occupancy

---
+++ tests/seeding/test.R
@@ -0,0 +1,7 @@
+args <- commandArgs(trailingOnly=TRUE)
+study <- data.frame(id = 1:5,
+                    sex = c("m", "m", "f", "f", "m"),
+                    score = c(51, 20, 67, 52, 42))
+Sys.sleep(600)
+dir.create("output")
+save(study, file="output/output.RData")
