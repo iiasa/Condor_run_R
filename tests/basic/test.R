eb42ac27afc8ccd9f88b2c0aa14666b04e09c392
Albert Brouwer <brouwer@iiasa.ac.at>
2021-04-16 18:29:49 +0200

    Remove superfluous output directory creation from tests

--- tests/basic/test.R
+++ tests/basic/test.R
@@ -4,5 +4,4 @@ study <- data.frame(id = 1:5,
                     sex = c("m", "m", "f", "f", "m"),
                     score = c(51, 20, 67, 52, 42))
 Sys.sleep(seconds_to_sleep)
-dir.create("output")
 save(study, file="output/output.RData")
