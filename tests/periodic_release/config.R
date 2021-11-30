478cb8c45e22edf031b03c503cf765a3d6264c75
Albert Brouwer <brouwer@iiasa.ac.at>
2021-11-30 15:43:56 +0100

    Update test configuration files

--- tests/periodic_release/config.R
+++ tests/periodic_release/config.R
@@ -1,14 +1,10 @@
-# Use paths relative to the working directory, with / as path separator.
-LABEL = "periodic_release_{Sys.Date()}" # label/name for your project/experiment, pick something short but descriptive without spaces and valid as part of a filename, can use {<config>} expansion here
+# See https://github.com/iiasa/Condor_run_R/blob/master/configuring.md
+LABEL = "periodic_release_{Sys.Date()}"
 JOBS = c(0:39,42,45)
-HOST_REGEXP = "^limpopo" # a regular expression to select execute hosts from the cluster
-REQUEST_MEMORY = 1000 # memory (MiB) to reserve for each job
-REQUEST_CPUS = 1 # number of hardware threads to reserve for each job
-LAUNCHER = "Rscript" # interpreter with which to launch the script
-SCRIPT = "test.R" # script that comprises your job
-ARGUMENTS = "%1" # arguments to the script
-BUNDLE_INCLUDE_DIRS = c("input") # recursive, supports wildcards
-BUNDLE_EXCLUDE_DIRS = c("Condor", "output") # recursive, supports wildcards
-BUNDLE_EXCLUDE_FILES = c("*.log") # supports wildcards
-BUNDLE_ADDITIONAL_FILES = c() # additional files to add to root of bundle, can also use an absolute path for these
+HOST_REGEXP = "^limpopo"
+REQUEST_MEMORY = 1000
+REQUEST_CPUS = 1
+LAUNCHER = "Rscript"
+SCRIPT = "test.R"
+ARGUMENTS = "%1"
 WAIT_FOR_RUN_COMPLETION = TRUE
--- tests/transport/config.R
+++ tests/transport/config.R
@@ -1,15 +1,16 @@
-LABEL = "transport_{Sys.Date()}" # label/name for your project/experiment, pick something short but descriptive without spaces and valid as part of a filename, can use {<config>} expansion here
-JOBS = c(0, 5, 10, 15, 20, 25, 30, 40, 45, 50) # New York demand shifters
-HOST_REGEXP = "^limpopo" # a regular expression to select execute hosts from the cluster
-REQUEST_MEMORY = 1000 # memory (MiB) to reserve for each job
-REQUEST_CPUS = 1 # number of hardware threads to reserve for each job
-GAMS_FILE_PATH = "transport.gms" # path to GAMS file to run for each job, relative to GAMS_CURDIR
-GAMS_ARGUMENTS = "gdx={GDX_OUTPUT_DIR}/{GDX_OUTPUT_FILE} //shift=%1" # additional GAMS arguments, can use {<config>} expansion here
-GAMS_VERSION = "32.2" # must be installed on all execute hosts
-G00_OUTPUT_DIR = "work" # directory for work/save file. Relative to GAMS_CURDIR both host-side and on the submit machine if G00_OUTPUT_DIR_SUBMIT is not set, excluded from bundle
-G00_OUTPUT_FILE = "work.g00" # name of work/save file. Host-side, will be remapped with LABEL and cluster/job numbers to avoid name collisions when transferring back to the submit machine.
-GET_G00_OUTPUT = TRUE # optional
-GDX_OUTPUT_DIR = "gdx" # relative to GAMS_CURDIR both host-side and on the submit machine if GDX_OUTPUT_DIR_SUBMIT is not set, excluded from bundle
-GDX_OUTPUT_FILE = "output.gdx" # as produced on the host-side by gdx= GAMS parameter or execute_unload, will be remapped with LABEL and cluster/job numbers to avoid name collisions when transferring back to the submit machine.
-GET_GDX_OUTPUT = TRUE # optional
+# See https://github.com/iiasa/Condor_run_R/blob/master/configuring.md
+LABEL = "transport_{Sys.Date()}"
+JOBS = c(0, 5, 10, 15, 20, 25, 30, 40, 45, 50)
+HOST_REGEXP = "^limpopo"
+REQUEST_MEMORY = 1000
+REQUEST_CPUS = 1
+GAMS_FILE_PATH = "transport.gms"
+GAMS_ARGUMENTS = "gdx={GDX_OUTPUT_DIR}/{GDX_OUTPUT_FILE} //shift=%1"
+GAMS_VERSION = "32.2"
+G00_OUTPUT_DIR = "work"
+G00_OUTPUT_FILE = "work.g00"
+GET_G00_OUTPUT = TRUE
+GDX_OUTPUT_DIR = "gdx"
+GDX_OUTPUT_FILE = "output.gdx"
+GET_GDX_OUTPUT = TRUE
 WAIT_FOR_RUN_COMPLETION = TRUE
