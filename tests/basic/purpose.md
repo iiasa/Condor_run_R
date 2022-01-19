a10b1f08ad0228029383e108fe7bd01c391e8eb5
Albert Brouwer <brouwer@iiasa.ac.at>
2022-01-19 11:40:51 +0100

    Expand test purpose with performance

--- tests/basic/purpose.md
+++ tests/basic/purpose.md
@@ -1,3 +1,3 @@
-Test basic run submission and result retrieval. Using `Condor_run_basic.R`
-simple R jobs are submitted. These sleep for some seconds and return a bit
-of data.
+Test basic run submission, host performance, and output retrieval.
+Using `Condor_run_basic.R` simple R jobs are submitted. These busy
+loop for a billlion iterations and return a bit of data.
--- tests/basic/test.R
+++ tests/basic/test.R
@@ -1,7 +1,12 @@
 args <- commandArgs(trailingOnly=TRUE)
-seconds_to_sleep <- as.integer(args[[1]])
+# Busy loop for a while
+sum <- 0
+for (i in 1:1e9) {
+  sum <- sum + cos(i)^2 + sin(i)^2
+}
+cat(sum, "\n")
+# Output dummy dataframe
 study <- data.frame(id = 1:5,
                     sex = c("m", "m", "f", "f", "m"),
                     score = c(51, 20, 67, 52, 42))
-Sys.sleep(seconds_to_sleep)
 save(study, file="output/output.RData")
