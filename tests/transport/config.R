b7eb5f412f685c8342797036205e3ced4336a8d7
Albert Brouwer <brouwer@iiasa.ac.at>
2022-08-17 09:39:08 +0200

    Revert REQUIREMENTS, already so set by default

--- tests/transport/config.R
+++ tests/transport/config.R
@@ -1,7 +1,6 @@
 # See https://github.com/iiasa/Condor_run_R/blob/master/configuring.md
 LABEL = "transport_{Sys.Date()}"
 JOBS = c(0, 5, 10, 15, 20, 25, 30, 40, 45, 50)
-REQUIREMENTS = c("GAMS")
 REQUEST_MEMORY = 1000
 REQUEST_DISK = 400
 GAMS_FILE_PATH = "transport.gms"
