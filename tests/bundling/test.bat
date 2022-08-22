4605709146a35466e72d636095466e17c57936b1
Albert Brouwer <brouwer@iiasa.ac.at>
2022-08-22 14:37:17 +0200

    Update test invocations

--- tests/bundling/test.bat
+++ tests/bundling/test.bat
@@ -4,5 +4,4 @@ Rscript ..\..\Condor_run_basic.R config.R || (
     popd
     exit /b 1
 )
-7z l Condor/**/_bundle*.7z
 popd
--- tests/bundling/test.sh
+++ tests/bundling/test.sh
@@ -2,5 +2,4 @@
 pushd "$(dirname "$0")"
 set -e
 Rscript ../../Condor_run_basic.R config.R
-7z l Condor/**/_bundle*.7z
 popd
