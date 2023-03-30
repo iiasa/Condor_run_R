a8122e12781de7cba33a6cc7e1b4e56d6f338d45
Albert Brouwer <brouwer@iiasa.ac.at>
2023-03-30 09:31:47 +0200

    Preserve working directory and check for error

--- tests/reserve_limpopo6_resources/reserve.bat
+++ tests/reserve_limpopo6_resources/reserve.bat
@@ -1,2 +1,9 @@
-condor_submit reserve.job
+@echo off
+pushd %~dp0
+condor_submit reserve.job || (
+    echo "Error: condor_submit failed."
+    popd
+    exit /b 1
+)
 echo "Warning: make sure that the job is running before trying to use the resources!"
+popd
--- tests/reserve_limpopo6_resources/reserve.sh
+++ tests/reserve_limpopo6_resources/reserve.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+pushd "$(dirname "$0")"
+set -e
 condor_submit reserve.job
 echo "Warning: make sure that the job is running before trying to use the resources!"
+popd
