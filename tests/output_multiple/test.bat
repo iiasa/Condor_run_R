202e932ac3d5dd8f48cce4f36074912179d62894
Albert Brouwer <brouwer@iiasa.ac.at>
2023-05-23 10:51:53 +0200

    Announce output listing

--- tests/output_multiple/test.bat
+++ tests/output_multiple/test.bat
@@ -5,5 +5,6 @@ Rscript ..\..\Condor_run_basic.R config.R || (
     popd
     exit /b 1
 )
+echo Contents of output directory:
 dir output
 popd
--- tests/output_multiple/test.sh
+++ tests/output_multiple/test.sh
@@ -3,5 +3,6 @@ pushd "$(dirname "$0")"
 set -e
 rm -f output/result*.???
 Rscript ../../Condor_run_basic.R config.R
+echo "Listing output directory:"
 ls output
 popd
