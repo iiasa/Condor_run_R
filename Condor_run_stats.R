72a3a3c1e1b68da58bdda6659fb011007f071286
Albert Brouwer <brouwer@iiasa.ac.at>
2024-10-16 10:21:56 +0200

    Bump release version

--- Condor_run_stats.R
+++ Condor_run_stats.R
@@ -7,7 +7,7 @@
 # Use:          https://github.com/iiasa/Condor_run_R#use
 #
 # Author:  Albert Brouwer
-# Release: https://github.com/iiasa/Condor_run_R/releases/tag/v2024-03-14
+# Release: https://github.com/iiasa/Condor_run_R/releases/tag/v2024-10-16
 
 # ---- Initialization ----
 
